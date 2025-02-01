6187f8ceb0bb198bf3644b3eb2227f9c3b79c04c
Ihr Name <ihre@email.com>
2025-02-01 15:37:54 +0100

    Fix: Restore Back to Home button in mobile menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -86,7 +86,7 @@ const Navbar = () => {
             >
               Features
             </button>
-            {isSecondaryPage && !isPrivacyPolicy && (
+            {isSecondaryPage && (
               <button
                 onClick={() => {
                   navigate('/');
